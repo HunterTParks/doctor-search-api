5980a9f70b667d1a6b3e26d55027774ddc2f0c5a
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-06-30 10:58:01 -0700

    add everything because I accidentally initialized my git folder into my js folder OOPS

---
+++ js/doctor-interface.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(function(){
+	var newDoctor = new Doctor();
+	$("#doctorSearch").submit(function(){
+		event.preventDefault();
+		var injury = $("#injury").val();
+		var location = $("#location").val();
+		newDoctor.GetDoctors(injury, location);
+	}
+
+});
