12bea4c53e76c17e152cb9232b01173d9189089e
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-06-30 10:34:13 -0700

    add constructor and prototype for finding doctors

--- doctor.js
+++ doctor.js
@@ -1,7 +1,11 @@
+var apiKey = require('./../.env').apiKey;
+
 Doctor = function(){
 	
 }
 
-Doctor.prototype.GetDoctors(response){
-	
+Doctor.prototype.GetDoctors(injury user_location){
+	$.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + injury + '&location=37.779&2C-122.413%2C100&user_location=' + user_location + '&skip=0&limit=10&user_key=' + apiKey).then(function(response){
+		
+	}
 }
