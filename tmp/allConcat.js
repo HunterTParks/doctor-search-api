749be2287b69c6a95320d64dd086a7e7aff30d9e
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-06-30 12:43:02 -0700

    add functionality to doctor-interface.js, being able to display doctors to front page after submission form

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -8,11 +8,11 @@ var Doctor = function(injury, location){
 };
 
 var DisplayInfo = function(response, i){
-	$('.displayInfo').append(response.data.practices[i].name);
+	$('.displayInfo').append('<p>' + response.data[i].profile.first_name + ' ' + response.data[i].profile.last_name + '</p>');
 };
 
 Doctor.prototype.GetDoctors = function(){
-	$.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + this.injury + '&location=' + this.location + '&user_location=37.773%2C-112.413&sort=full-name-desc&skip=0&limit=10&user_key=c' + apiKey).then(function(response){
+	$.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + this.injury + '&location=' + this.location + '&user_location=45.5231%2C%20122.6765&sort=full-name-desc&skip=0&limit=10&user_key=' + apiKey).then(function(response){
 		for(var i = 1; i <= 10; i++){
 			console.log(response);
 			DisplayInfo(response, i);
